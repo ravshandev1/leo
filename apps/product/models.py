72db6c99d7ffc79e5129e357b51b16e7628f527c
Ravshan <ravshangiyosov2002@gmail.com>
2024-09-09 01:17:20 +0500

    17

--- apps/product/models.py
+++ apps/product/models.py
@@ -14,7 +14,7 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey(Category, models.CASCADE, 'products')
     name = models.CharField(max_length=250, unique=True)
-    bonus = models.OneToOneField(Bonus, models.CASCADE, related_name='products', limit_choices_to={''})
+    bonus = models.OneToOneField(Bonus, models.CASCADE, related_name='products')
     price = models.IntegerField()
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
