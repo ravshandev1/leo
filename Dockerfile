c85598178f8149315645abe71fb85050d2e317f3
Ravshan <ravshangiyosov2002@gmail.com>
2024-09-09 17:15:21 +0500

    21

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ WORKDIR /app
 ADD requirements.txt .
 RUN pip install -r requirements.txt
 ADD . .
-RUN sed -i 's/^DEBUG=.*/DEBUG="0"/' .env
+#RUN sed -i 's/^DEBUG=.*/DEBUG="0"/' .env
 RUN sed -i 's/^DB_HOST=.*/DB_HOST="leo.ravshandev.uz"/' .env
 EXPOSE 8000
 CMD ["sh","-c", "python manage.py migrate && python manage.py runserver 0.0.0.0:8000"]
--- apps/user/utils.py
+++ apps/user/utils.py
@@ -7,11 +7,11 @@ def send_verification_code(phone: str, code: str):
     token = request("post", 'https://notify.eskiz.uz/api/auth/login',
                     data={'email': settings.SMS_EMAIL, 'password': settings.SMS_PASSWORD})
     headers = {
-        # "Authorization": f"Bearer {token.json()['data']['token']}"
+        "Authorization": f"Bearer {token.json()['data']['token']}"
     }
     data = {
         'mobile_phone': phone,
-        'message': f"Confirmation code from Leo: {code}",
+        'message': f"Leo Usta Bot ro'yxatdan o'tish uchun tasdiqlash kodi: {code}\nЛео мастер бот код подтверждения регистрации: {code}",
         'from': "4546",
     }
     request("post", 'https://notify.eskiz.uz/api/message/sms/send', data=json.dumps(data), headers=headers)
