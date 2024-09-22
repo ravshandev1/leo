17809763d3fb6fa690a11a537ebf722eae6a9978
Ravshan <ravshangiyosov2002@gmail.com>
2024-09-23 00:24:03 +0500

    full

--- task.sh
+++ task.sh
@@ -6,10 +6,8 @@ import datetime
 from django.utils import timezone
 from django.conf import settings
 import requests
-
 from user.models import UserPoint
 
-# Telegram bot token and chat URL
 TELEGRAM_API_URL = f'https://api.telegram.org/bot{settings.BOT_TOKEN}/sendMessage'
 
 def send_telegram_message(chat_id, message):
@@ -24,16 +22,19 @@ def process_user_points():
     one_year_ago = timezone.now() - datetime.timedelta(days=365)
     one_week_left = timezone.now() - datetime.timedelta(days=365 - 7)
 
-    # Find UserPoints that are 1 year old and subtract bonus points
     expired_points = UserPoint.objects.filter(created_at__lte=one_year_ago)
     for point in expired_points:
-        point.user.point -= point.bonus.point
+        point.user.summa -= point.bonus.summa
         point.user.save()
 
-    # Find UserPoints with 1 week remaining and send Telegram reminder
     points_soon_to_expire = UserPoint.objects.filter(created_at__range=[one_week_left, one_year_ago])
     for point in points_soon_to_expire:
-        message = 'Balingizni foydalaning, yuqsa 1 haftadan keyin o\'chib ketadi.'
+        message_uz = 'Balingizni foydalaning, yuqsa 1 haftadan keyin o\'chib ketadi.'
+        message_ru = 'Balingizni foydalaning, yuqsa 1 haftadan keyin o\'chib ketadi.'
+        if point.user.lang == 'uz':
+            message = message_uz
+        else:
+            message = message_ru
         send_telegram_message(point.user.chat_id, message)
 process_user_points()
 " | python manage.py shell
